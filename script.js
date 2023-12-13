20dda30e2c0c9ad5129b033af662db88e674a94e
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-13 20:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 const makeChange = (c) => {
   // your name here
+	let m=c/100;
+	let pn=0.01,ni=0.05,dime=0.1,qu=0.25;
+	let c1=0,c2=0,c3=0,c4=0;
+	while(m>=qu){
+		c1++;
+		m=m-qu;
+	}
+	while(m>=dime){
+		c2++;
+		m=m-dime;
+	}
+	while(m>=ni){
+		c3++;
+		m=m-ni;
+	}
+	while(m>=pn){
+		c4++;
+		m=m-pn;
+		       if(m>=0.005 && m<=0.01) m=0.01;
+
+	}
+	let obj={
+		q:c1,
+		d:c2,
+		n:c3,
+		p:c4,
+	}  
+	return obj;
 };
 
 // Do not the change the code below
